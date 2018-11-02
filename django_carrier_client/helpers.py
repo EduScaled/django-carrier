bc33c05a5866d7e47284e7ba55cfa2885efd6db7
Nick Lubyanov <lubyanov@gmail.com>
2018-11-02 17:22:26 +0300

    singleton

--- django_carrier_client/helpers.py
+++ django_carrier_client/helpers.py
@@ -1,13 +1,18 @@
-class MessageManagerHelperClass():
+class MessageManagerHelperClass:
+    instance = None
+
+    class __MessageManagerHelperClass:
+        def __init__(self):
+            self._to_listen = []
 
     def __init__(self):
-        self._to_listen = []
+        if not MessageManagerHelperClass.instance:
+            MessageManagerHelperClass.instance = MessageManagerHelperClass.__MessageManagerHelperClass()
 
     def set_manager_to_listen(self, manager):
-        self._to_listen.append(manager)   
+        self.instance._to_listen.append(manager)   
         
     def get_message_managers_to_listen(self):
-        return self._to_listen 
+        return self.instance._to_listen 
 
-# TODO singleton
 MessageManagerHelper = MessageManagerHelperClass()
